89071a1c423e735b5eec75267889820da4168875
Danil Chernikov <danil.chernikov@onix-systems.com>
2023-12-09 13:52:20 +0100

    Initial Commit

---
+++ SwiftUI_Challenge4/SwiftUI_Challenge4App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Challenge4App.swift
+//  SwiftUI_Challenge4
+//
+//  Created by Danil Chernikov on 09.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Challenge4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
